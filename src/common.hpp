f9118512f7012fa0bf89a6e02dec4432e7ecaa5c
EdmondDantes21 <redi.vreto@studenti.unitn.it>
2025-01-15 19:43:48 +0100

    removed N to be able to adjust id dynamically

--- src/common.hpp
+++ src/common.hpp
@@ -2,8 +2,19 @@
 #define COMMON_HPP
 
 #define DIMENSIONS 4
-#define N_HYPERPLANES 8
-#define N 128
-#define BUCKET_SIZE 8
+#define N_HYPERPLANES 128
+#define BUCKET_SIZE 4
+
+// Macro for error checking
+#define CHECK_CUDA(call) do {                              \
+    cudaError_t err = call;                               \
+    if (err != cudaSuccess) {                             \
+        std::cerr << "CUDA error in " << __FILE__        \
+                  << " at line " << __LINE__             \
+                  << ": " << cudaGetErrorString(err)    \
+                  << std::endl;                          \
+        exit(err);                                        \
+    }                                                      \
+} while (0)
 
 #endif
